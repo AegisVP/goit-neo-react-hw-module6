ea0a6f8fd48ec27e2f2043ca1274b01159addc95
Vlad P <vlad@pysarenko.com>
2024-08-29 00:41:33 +0200

    hide filter box if no contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,22 @@ import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
 
 export default function App() {
+  const contactsList = useSelector(selectContacts);
   return (
     <div className={css.app}>
       <h1>Phonebook</h1>
       <div className={css.card}>
         <ContactForm />
       </div>
-      <div className={css.card}>
-        <SearchBox />
-      </div>
+      {contactsList.length > 0 && (
+        <div className={css.card}>
+          <SearchBox />
+        </div>
+      )}
       <div className={css.list}>
         <ContactList />
       </div>
